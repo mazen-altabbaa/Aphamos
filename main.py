b3303c6312b64027a085b16b8b58477dad36fab0
mazen <mazenaltabbaa366@gmail.com>
2025-12-19 21:30:14 +0300

    Restructure project folders: remove Result and add models directories

--- main.py
+++
@@ -1,9 +0,0 @@
-import os, cv2, json, pickle
-import numpy as np
-from pathlib import Path
-from tqdm import tqdm
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from transformers import CLIPProcessor, CLIPModel
-import torch
-from PIL import Image
